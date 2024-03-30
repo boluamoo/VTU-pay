31fefceb06111ea69766278a0553fdc8312648f1
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-03-30 09:47:47 +0100

    finished signup 1

---
+++ sign-in/index.js
@@ -0,0 +1,8 @@
+function togglePasswordVisibility() {
+  const password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
